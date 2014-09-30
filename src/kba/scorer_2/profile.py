ca0285ebd1ef78606b3fb0b67555aff41f6ef9f3
Josh Belanich <josh@diffeo.com>
2014-09-30 22:08:08 +0000

    Added profiles_from_profilefile and profiles_from_runfile, that will read a (profile|runfile)file, and return a dict mapping between profile naames and ScorableProfile.
    Added a scoring function.

--- src/kba/scorer_2/profile.py
+++ src/kba/scorer_2/profile.py
@@ -15,10 +15,10 @@ class ScorableProfile(object):
         profile_file = open(profile_path, 'r')
         
         import yaml
-        profiles = yaml.load(profile_file)
+        profiles_yaml = yaml.load(profile_file)
 
-        entities = profiles['entities']
-        profiles = defaultdict(ScorableProfile)
+        entities = profiles_yaml['entities']
+        profiles = dict()
         for ent_name,ent_data in entities.iteritems():
             profile = ScorableProfile(ent_name)
             for slot_name,slot_values in ent_data['slots'].iteritems():
@@ -30,7 +30,7 @@ class ScorableProfile(object):
                     for slot_value in slot_values:
                         profile.add_value_for_slot(slot_name, slot_value['value'])
                 else:
-                    profile.add_value_for_slot(slot_name, slot_values)
+                    profile.add_value_for_slot(slot_name, slot_values.lower())
 
             profiles[ent_name] = profile
 
@@ -43,7 +43,29 @@ class ScorableProfile(object):
         Go through the runfile and build profiles by collapsing the
         temporal data into single profiles.
         '''
-        pass
+        runfile = open(runfile_path, 'r')
+        
+        #skip comment at beginning of file
+        next(runfile)
+
+        import csv
+        profiles = dict()
+        csv_reader = csv.reader(runfile, delimiter='\t')
+        for row in csv_reader:
+            if row[0].startswith('#'):
+                continue
+            profile_name = row[3]
+            slot_name = row[8]
+            slot_value = row[9]
+
+            if profile_name not in profiles:
+                profiles[profile_name] = ScorableProfile(profile_name, truncate_counts = True)
+
+            profiles[profile_name].add_value_for_slot(slot_name, slot_value.lower().replace('_',' '))
+
+        return profiles
+        
+        
     
     def __init__(self, profile_name, truncate_counts = False):
         #slot_name -> StringCounter
@@ -74,11 +96,56 @@ class ScorableProfile(object):
         score_sum = 0.0
         for slot_name, slot_value in self._slots.iteritems():
             other_slot_value = other._slots[slot_name] #defaults to empty StringCounter
-            score_sum += vector.dot(slot_value, other_slot_value)
+            if method == 'sokalsneath':
+                score_sum += sokalsneath(slot_value, other_slot_value)
+            else:
+                score_sum += vector.dot(slot_value, other_slot_value)
 
         return score_sum/len(self._slots.keys())
+
+def sokalsneath(sc1, sc2):
+    '''
+    Runs sokalsneath kernel on two StringCounters
+    '''
+    both = 0
+    anotb = 0
+    bnota = 0
+    for key in sc1:
+        if key in sc2:
+            both += 1
+        else:
+            anotb += 1
+
+    for key in sc2:
+        if key in sc2:
+            #skip
+            pass
+        else:
+            bnota += 1
+
+    R = 2 * (anotb + bnota)
+    if both + R == 0:
+        #this means that both s1 and s2 were empty counters
+        return 0
+
+    return float(both) / (both + R)
+
+def score_run(runfile_path, profiles, method='cosine'):
+    from_runfile = ScorableProfile.profiles_from_runfile(runfile_path)
+
+    score_sum = 0.0
+    for entity in from_runfile.keys():
+        runfile = from_runfile[entity]
+        profile = profiles[entity]
+        
+        score_sum += runfile.compare(profile, method=method)
+
+    return score_sum / len(from_runfile.keys())
         
 if __name__ == '__main__':
-    test = ScorableProfile.profiles_from_profilefile('/home/josh/git/KBA/2014/judgments/trec-kba-2014-07-11-ccr-and-ssf.profiles.yaml')
-    first,second = test.keys()[0],test.keys()[1]
-    test[first].compare(test[first])
+    scores = []
+    profiles = ScorableProfile.profiles_from_profilefile('/home/josh/git/KBA/2014/judgments/trec-kba-2014-07-11-ccr-and-ssf.profiles.yaml')
+    for suffix in xrange(1,15):
+        score = score_run('data/SCU-ssf_{}'.format(suffix), profiles, method='sokalsneath')
+        scores.append(score)
+    print scores
